3fb11e2e0a6eac4ac6092b90e8b62cd3a8cbace1
Abraham Tesfay <Abraham Tesfay@DESKTOP-P5ERFD0>
2016-10-01 10:35:06 -0400

    Uncommenting code that was commented out for faster response while testing dropping columns

--- sparkmaster/src/main/scala/learn/spark/mapr/lab5/Incidents.scala
+++ sparkmaster/src/main/scala/learn/spark/mapr/lab5/Incidents.scala
@@ -39,11 +39,11 @@ object Incidents {
     val PdId = 11
 
     val incidentsRDD = sparkSession.sparkContext.textFile("file:///C:/Downloads/MapR/DEV3600_LAB_DATA/data/sfpd.csv").map(line => line.split(","));
-//    incidentsRDD.first().foreach(println);
-//    println(incidentsRDD.count());
-//    val categories = incidentsRDD.map(incident => incident(Category)).distinct().collect().sortBy(cat => cat)
-//    println("------------->> CATEGORIES ---------------------")
-//    categories.foreach(println);
+    incidentsRDD.first().foreach(println);
+    println(incidentsRDD.count());
+    val categories = incidentsRDD.map(incident => incident(Category)).distinct().collect().sortBy(cat => cat)
+    println("------------->> CATEGORIES ---------------------")
+    categories.foreach(println);
 
     //Create DataFrame Using Reflection to Infer Schema
     import sparkSession._
@@ -56,24 +56,24 @@ object Incidents {
     // register it as a temp table so that we can run queries on it
     incidentsDF.createOrReplaceTempView("incidents")
 
-//    // show top 5 Districts with highest number of incidents
-//    val districtsWithTopIncidents = sparkSession.sqlContext.sql("SELECT PdDistrict, count(IncidentNum) as incidentCount FROM "
-//      + " incidents GROUP BY PdDistrict ORDER BY incidentCount DESC LIMIT 5");
-//
-//    districtsWithTopIncidents.show()
-//
-//    // show top 10 resolutions
-//    val top10ResolutionsDF = sparkSession.sqlContext.sql("SELECT Resolution, count(Resolution) as resolutionCounts FROM "
-//      + "incidents GROUP BY Resolution ORDER BY resolutionCounts DESC LIMIT 10");
-//    top10ResolutionsDF.show();
-//
-//    // show top 10 categories resolutions
-//    sparkSession.sqlContext.sql("SELECT Category, count(Category) as categoryCounts FROM "
-//      + "incidents GROUP BY Category ORDER BY categoryCounts DESC LIMIT 10").show();
-//
-//    // save top 10 resolutions to a JSON file
-//    println("-------------------------->>>> Saving 10 top resolutions to JSON file ...")
-//    top10ResolutionsDF.write.format("json").mode(SaveMode.Overwrite).save(warehouseLocation + "/Resolutions");
+    // show top 5 Districts with highest number of incidents
+    val districtsWithTopIncidents = sparkSession.sqlContext.sql("SELECT PdDistrict, count(IncidentNum) as incidentCount FROM "
+      + " incidents GROUP BY PdDistrict ORDER BY incidentCount DESC LIMIT 5");
+
+    districtsWithTopIncidents.show()
+
+    // show top 10 resolutions
+    val top10ResolutionsDF = sparkSession.sqlContext.sql("SELECT Resolution, count(Resolution) as resolutionCounts FROM "
+      + "incidents GROUP BY Resolution ORDER BY resolutionCounts DESC LIMIT 10");
+    top10ResolutionsDF.show();
+
+    // show top 10 categories resolutions
+    sparkSession.sqlContext.sql("SELECT Category, count(Category) as categoryCounts FROM "
+      + "incidents GROUP BY Category ORDER BY categoryCounts DESC LIMIT 10").show();
+
+    // save top 10 resolutions to a JSON file
+    println("-------------------------->>>> Saving 10 top resolutions to JSON file ...")
+    top10ResolutionsDF.write.format("json").mode(SaveMode.Overwrite).save(warehouseLocation + "/Resolutions");
     
     println("-----------------------Additional Testing -----------------------------------")
     incidentsDF.take(10).foreach { println };
